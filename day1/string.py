021bfc74c755f958f4a1060c5db1176ef5113418
shenyuan <yuanshen106@qq.com>
2016-08-18 11:45:18 +0800

    update用户登录3次

---
+++ day1/string.py
@@ -0,0 +1,19 @@
+#!user/bin/env python
+#-*-coding:utf-8 -*-
+# Author: Sy106
+
+# name='sy106,sy107,sy108'
+# print name
+# name2=name.split(',')
+# print name2
+# name3=('!'.join(name2))
+# print name3
+#
+name4 = "alex li"
+print""in name4
+print name4.center(40,'-')
+# print name4.capitalize()
+
+msg="Hello ,{name},it's been a long {age} since last time spoke..."
+msg1=msg.format(name='sy106',age=333)
+print msg1
