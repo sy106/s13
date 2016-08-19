788982f684da535705285d56077b98cb1ee66fc7
shenyuan <yuanshen106@qq.com>
2016-08-19 17:53:02 +0800

    输入用户名和密码登录 输入正确的用户名后，才能输入密码 输错三次就锁定用户名

--- day1/login.py
+++ day1/login.py
@@ -4,3 +4,54 @@
 
 import os
 
+#write userinfo into local file
+f=file('userinfo.txt','a')
+f.write('sy106 123\n')
+f.write('admin admin\n')
+f.write('alex 123\n')
+f.close()
+
+#read userinfo
+account='user.txt'
+f=file(account)
+accountName=f.readlines()
+print 'the Auth Names are:',accountName
+
+for i in range(3):
+    Login=False
+    #read lock name into local file
+    username=raw_input('please input your login name:').strip()
+    lock='lock.txt'
+    f=file(lock)
+    lockName_list=[]
+    for lockName in f.readlines():
+        lockName=lockName.strip('\n')
+        lockName_list.append(lockName)
+    f.close()
+    print 'lockName_list are:',lockName_list
+    if username in lockName_list:
+        print'The user %s has been lock!please use another username!'%username
+        break
+    for line in accountName:
+        line=line.split()
+        if username==line[0]:#if the name is right ,input the passwd
+            for i in range(3):
+                passwd = raw_input('please input your password:').strip()
+                if passwd==line[1]:
+                    print "Welcome %s login the system!"%username#login success
+                    Login = True
+                    break
+            else:
+                f=file(lock,'a')
+                f.write('%s\n'%username)
+                f.close()
+                print 'Enter 3 times wrong password,the name %s is locked'%username
+            if Login is True:break#jump out of for loop
+
+    if  Login is True:
+        break#jump out of top for loop
+
+
+
+
+
--- day1/作业1.py
+++ day1/作业1.py
@@ -19,11 +19,28 @@ for i in range(3):
     username = raw_input("please input your username:").strip()
     password = raw_input("please input your password:").strip()
     if len(username)!=0 and len(password)!=0:
-        f = file(account)
+        f1 = file(account)
         loginOK = False#login 标志位
-        Names=f.readlines()#读出有效用户名和密码
+        Names=f1.readlines()#读出有效用户名和密码
+        print 'the Auth Names are:',Names
         for Auth in Names:
             Auth=Auth.split()
+        #     print'Auth',Auth
+        # f2 = file(lock)
+        # lockNames = f2.readlines()#读出锁定的用户名
+        # Len = len(lockNames)
+        # print 'the lock names are:', lockNames
+        # print 'the lock len are:', Len
+        # print 'the type of lockNames:',type(lockNames)
+        # print 'the type of username:', type(username)
+        # Name=username+'\n'
+        # print 'Name:', Name
+        # for x in range(Len):
+        #     if lockNames[x]==Name:
+        #     # print 'Lock:', lockName
+        #     # if username==lockName[0]:
+        #         print 'the %s is lock,please exchage another name!' % username
+        #         break
 
             if username==Auth[0] and password==Auth[1]:#username and password are correct
                 print "Welcome %s login my system!" %username
@@ -35,18 +52,22 @@ for i in range(3):
                 print 'Wrong password! please retry!'
                 break
             if username!=Auth[0] :  #username is wrong
-
                  WrongName.append(username)
                  WrongTime = WrongTime + 1
                  print 'wrong name is:',WrongName
                  print 'wrong time is:', WrongTime
                  print WrongName.count(username)
+
                  if WrongName.count(username)==3:
-                     print WrongName
-                     a = file("lock.txt",'w')
-                     a.write(username)+('\n')
-                     a.close()
+                     print  'the lock name is',WrongName
+                     a = file("lock.txt",'a')
+
+                     a.write('%s\n'%username)
+                     f.close()
+                     print 'tpye of name',type(username)
                      os.system("more lock.txt")
+
+
                  break
 
 
