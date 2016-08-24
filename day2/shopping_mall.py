560dd22f318fde6df6c3ba550402465ef5030eae
shenyuan <yuanshen106@qq.com>
2016-08-24 17:28:10 +0800

    第二周作业： 1. 商品信息- 数量、单价、名称 2. 用户信息- 帐号、密码、余额 3. 用户可充值 4. 购物历史信息 5. 允许用户多次购买，每次可购买多件 6. 余额不足时进行提醒 7. 用户退出时 ，输出档次购物信息 8. 用户下次登陆时可查看购物历史 9. 商品列表分级显示

--- day2/shopping_mall.py
+++ day2/shopping_mall.py
@@ -1,14 +1,63 @@
-#!user/bin/env python
-#-*-coding:utf-8 -*-
-# Author: Sy106
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Author: Sy106
 
-salary=input('please input your salary:').strip()
+salary = input("Input your salary:")
 if salary.isdigit():
-    salary=int(salary)
-    print ('your salary is:%d'%salary)
+    salary = int(salary)
 else:
-    exit('invalid data type..')
+    exit("Invaild data type...")
 
-welcom_msg='''wlecome to sy106 shopping mall!'''.center(60,'-')
+welcome_msg = 'Welcome to Sy106 Shopping mall'.center(50,'-')
+print(welcome_msg)
 
-print(welcom_msg)
+exit_flag = False
+product_list = [
+    ('Iphone',5888),
+    ('Mac Air',8000),
+    ('Mac Pro',9000),
+    ('XiaoMi 2',19.9),
+    ('Coffee',30),
+    ('Tesla',820000),
+    ('Bike',700),
+    ('Cloth',200),]
+
+shop_car = []
+while  exit_flag is not True:
+    #for product_item in product_list:
+    #    p_name,p_price = product_item
+    print("product list".center(50,'-'))
+    for item in enumerate(product_list):
+        index = item[0]
+        p_name = item[1][0]
+        p_price = item[1][1]
+        print(index,'.',p_name,p_price)
+
+    user_choice = input("[q=quit,c=check]What do you want to buy?:")
+    if user_choice.isdigit():#鑲畾鏄€夋嫨shangpin
+        user_choice = int(user_choice)
+        if user_choice < len(product_list):
+            p_item = product_list[user_choice]
+            if p_item[1] <= salary: #涔扮殑璧?
+                shop_car.append(p_item) #鏀惧叆璐墿杞?
+                salary -= p_item[1] #鍑忛挶
+                print("Added [%s] into shop car,you current balance is \033[31;1m[%s]\033[0m" %
+                      (p_item,salary))
+            else:
+                print("Your balance is [%s],cannot afford this.." % salary)
+    else:
+        if user_choice == 'q' or user_choice =='quit':
+            print("purchased products as below".center(40,'*'))
+            for item in shop_car:
+                print(item)
+            print("END".center(40,'*'))
+            print("Your balance is [%s]" % salary)
+            print("Bye")
+            exit_flag = True
+        elif  user_choice == 'c' or user_choice =='check':
+            print("purchased products as below".center(40,'*'))
+            for item in shop_car:
+                print(item)
+            print("END".center(40,'*'))
+            print("Your balance is \033[41;1m[%s]\033[0m" % salary)
+
