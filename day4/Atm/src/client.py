c041c0b9002e87ff83ceee7e1e529f4c37922114
yuanshen <yuanshen106@qq.com>
2016-09-16 02:15:59 +0800

    ATM取款机

--- day4/Atm/src/client.py
+++ day4/Atm/src/client.py
@@ -10,13 +10,10 @@ from day4.Atm.src.backend import logger
 
 CURRENT_USER_INFO = {}
 
+
 def dump_current_user_info():
 
     json.dump(CURRENT_USER_INFO, open(os.path.join(settings.USER_DIR_FOLDER, CURRENT_USER_INFO['card'], "basic_info.json"), 'w'))
-
-def dump_transfer_user_info():
-
-    json.dump(TRANSFER_USER_INFO, open(os.path.join(settings.USER_DIR_FOLDER, TRANSFER_USER_INFO['card'], "basic_info.json"), 'w'))
 
 def write_record(message):
     """
@@ -56,6 +53,7 @@ def repay():
     还款
     :return:
     """
+    REPAY = False
     leng = len(CURRENT_USER_INFO['debt'])
     msg = """
            卡号：              %s
@@ -65,21 +63,102 @@ def repay():
            当前储蓄账户余额：   %s
            当前信用卡剩余额度： %s
         """
-    for i in range(leng):
-        print(msg % (CURRENT_USER_INFO['card'], CURRENT_USER_INFO['debt'][i]['date'], CURRENT_USER_INFO['debt'][i]['total_debt'],
-                     CURRENT_USER_INFO['debt'][i]['balance_debt'], CURRENT_USER_INFO['saving'],
+    for k in range(leng):
+        print(msg % (CURRENT_USER_INFO['card'], CURRENT_USER_INFO['debt'][k]['date'], CURRENT_USER_INFO['debt'][k]['total_debt'],
+                     CURRENT_USER_INFO['debt'][k]['balance_debt'], CURRENT_USER_INFO['saving'],
                      CURRENT_USER_INFO['balance']))
     date=input("请输入需要还款的日期:例如2016_4_10\n>>>").strip()
-    num = float(input('请输入还款金额').strip())
-
+    num = float(input('请输入还款金额:>>>').strip())
+    if num<=0:
+        exit("还款金额必须大于0！")
     for i in range(leng):
-        if date == CURRENT_USER_INFO['debt'][i]['date']:
-            CURRENT_USER_INFO['debt'][i]['balance_debt']-=num
-            CURRENT_USER_INFO['debt'][i]['total_debt']-=num
-            #重新load数据文件，如果还款金额大于账单金额，就冲抵目前的信用卡差额，再多，就存到储蓄账户里
+        if date == CURRENT_USER_INFO['debt'][i]['date']:#重新load数据文件，如果还款金额大于账单金额，就冲抵目前的信用卡差额，再多，就存到储蓄账户里
+            if num <= CURRENT_USER_INFO['debt'][i]['balance_debt']:
+                CURRENT_USER_INFO['debt'][i]['balance_debt']-=num
+                CURRENT_USER_INFO['debt'][i]['total_debt']-=num
+                write_record('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-目前储蓄账户余额：%s；信用卡账户额度：%s；'
+                             % ("还款",date,CURRENT_USER_INFO['debt'][i]['balance_debt'],CURRENT_USER_INFO['debt'][i]['total_debt'],CURRENT_USER_INFO['saving'],CURRENT_USER_INFO['balance']))
+                print('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-目前储蓄账户余额：%s；信用卡账户额度：%s；'
+                             % ("还款",date,CURRENT_USER_INFO['debt'][i]['balance_debt'],CURRENT_USER_INFO['debt'][i]['total_debt'],CURRENT_USER_INFO['saving'],CURRENT_USER_INFO['balance']))
+                REPAY = True
+                break
+            elif num>CURRENT_USER_INFO['debt'][i]['balance_debt'] and num<=CURRENT_USER_INFO['debt'][i]['total_debt']:
+                CURRENT_USER_INFO['debt'][i]['total_debt'] -= num-CURRENT_USER_INFO['debt'][i]['balance_debt']
+                CURRENT_USER_INFO['debt'][i]['balance_debt'] = 0
+                write_record('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-目前储蓄账户余额：%s；信用卡账户额度：%s；'
+                             % ("还款", date, CURRENT_USER_INFO['debt'][i]['balance_debt'],
+                                CURRENT_USER_INFO['debt'][i]['total_debt'], CURRENT_USER_INFO['saving'],
+                                CURRENT_USER_INFO['balance']))
+                print('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-目前储蓄账户余额：%s；信用卡账户额度：%s；'
+                             % ("还款", date, CURRENT_USER_INFO['debt'][i]['balance_debt'],
+                                CURRENT_USER_INFO['debt'][i]['total_debt'], CURRENT_USER_INFO['saving'],
+                                CURRENT_USER_INFO['balance']))
+                REPAY = True
+                break
+            else:
+                if (num-CURRENT_USER_INFO['debt'][i]['total_debt']+float(CURRENT_USER_INFO['balance']))<=float(CURRENT_USER_INFO['credit']):#还款没有超过信用卡额度
+                    CURRENT_USER_INFO['balance'] += num - CURRENT_USER_INFO['debt'][i]['total_debt']
 
-    # write_record('%s - 储蓄账户：%f；信用卡账户：%f；' % ("还款",CURRENT_USER_INFO['saving'],num))
-    # CURRENT_USER_INFO['balance'] += num
+                    write_record('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-信用卡额度新增：%s；-储蓄账户余额：%s；信用卡账户额度：%s；'
+                                 % ("还款", date, CURRENT_USER_INFO['debt'][i]['balance_debt'],
+                                    CURRENT_USER_INFO['debt'][i]['total_debt'],
+                                    num - CURRENT_USER_INFO['debt'][i]['total_debt'], CURRENT_USER_INFO['saving'],
+                                    CURRENT_USER_INFO['balance']))
+                    print('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-信用卡额度新增：%s；-储蓄账户余额：%s；信用卡账户额度：%s；'
+                                 % ("还款", date, CURRENT_USER_INFO['debt'][i]['balance_debt'],
+                                    CURRENT_USER_INFO['debt'][i]['total_debt'],
+                                    num - CURRENT_USER_INFO['debt'][i]['total_debt'], CURRENT_USER_INFO['saving'],
+                                    CURRENT_USER_INFO['balance']))
+                    CURRENT_USER_INFO['debt'][i]['balance_debt'] = 0
+                    CURRENT_USER_INFO['debt'][i]['total_debt'] = 0
+                    REPAY = True
+                    break
+                else:#还款超过了信用卡额度，放到储蓄余额里
+                    num2 = float(CURRENT_USER_INFO['credit']) - float(CURRENT_USER_INFO['balance'])
+                    CURRENT_USER_INFO['saving'] += num - CURRENT_USER_INFO['debt'][i]['total_debt'] - num2
+                    test = num - CURRENT_USER_INFO['debt'][i]['total_debt']
+                    CURRENT_USER_INFO['balance'] = CURRENT_USER_INFO['credit']
+                    write_record('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-储蓄账户余额：%s；信用卡账户额度：%s；-储蓄卡新增：%f'
+                                 % ("还款",date,CURRENT_USER_INFO['debt'][i]['balance_debt'],CURRENT_USER_INFO['debt'][i]['total_debt'],CURRENT_USER_INFO['saving'],CURRENT_USER_INFO['balance'],test))
+                    print('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-储蓄账户余额：%s；信用卡账户额度：%s；-储蓄卡新增：%f'
+                                 % ("还款",date,CURRENT_USER_INFO['debt'][i]['balance_debt'],CURRENT_USER_INFO['debt'][i]['total_debt'],CURRENT_USER_INFO['saving'],CURRENT_USER_INFO['balance'],test))
+                    CURRENT_USER_INFO['debt'][i]['balance_debt'] = 0
+                    CURRENT_USER_INFO['debt'][i]['total_debt'] = 0
+                    REPAY = True
+                    break
+
+                    #
+                    # if (num+float(CURRENT_USER_INFO['balance']))<=float(CURRENT_USER_INFO['credit']):
+                    #     CURRENT_USER_INFO['balance'] += num-CURRENT_USER_INFO['debt'][i]['total_debt']
+                    #     CURRENT_USER_INFO['debt'][i]['balance_debt'] = 0
+                    #     CURRENT_USER_INFO['debt'][i]['total_debt'] = 0
+                    #     write_record('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-信用卡额度新增：%s；-储蓄账户余额：%s；信用卡账户额度：%s；'
+                    #                  % ("还款", date, CURRENT_USER_INFO['debt'][i]['balance_debt'],
+                    #                     CURRENT_USER_INFO['debt'][i]['total_debt'],
+                    #                     num - CURRENT_USER_INFO['debt'][i]['total_debt'], CURRENT_USER_INFO['saving'],
+                    #                     CURRENT_USER_INFO['balance']))
+                    #     print('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-信用卡额度新增：%s；-储蓄账户余额：%s；信用卡账户额度：%s；'
+                    #                  % ("还款", date, CURRENT_USER_INFO['debt'][i]['balance_debt'],
+                    #                     CURRENT_USER_INFO['debt'][i]['total_debt'],
+                    #                     num - CURRENT_USER_INFO['debt'][i]['total_debt'], CURRENT_USER_INFO['saving'],
+                    #                     CURRENT_USER_INFO['balance']))
+                    #     REPAY = True
+                    #     break
+                    #
+                    # else:
+                    #     CURRENT_USER_INFO['saving'] += num
+                    #     write_record('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-储蓄卡新增：%s；-储蓄账户余额：%s；信用卡账户额度：%s；'
+                    #              % ("还款",date,CURRENT_USER_INFO['debt'][i]['balance_debt'],CURRENT_USER_INFO['debt'][i]['total_debt'],num - CURRENT_USER_INFO['debt'][i]['total_debt'],CURRENT_USER_INFO['saving'],CURRENT_USER_INFO['balance']))
+                    #     print('%s - 还款账单日期：%s；-需要还款信用卡账单余额：%f；-需要还款总账户余额：%f；-储蓄卡新增：%s；-储蓄账户余额：%s；信用卡账户额度：%s；'
+                    #              % ("还款",date,CURRENT_USER_INFO['debt'][i]['balance_debt'],CURRENT_USER_INFO['debt'][i]['total_debt'],num - CURRENT_USER_INFO['debt'][i]['total_debt'],CURRENT_USER_INFO['saving'],CURRENT_USER_INFO['balance']))
+                    #     REPAY = True
+                    #     break
+        elif REPAY==True:
+            break
+
+    else:
+        print("该日期没有账单！请重新输入！")
+
     dump_current_user_info()
 
 
@@ -137,7 +216,7 @@ def transfer():
             temp = num - CURRENT_USER_INFO['saving']
             CURRENT_USER_INFO['balance'] = float(CURRENT_USER_INFO['balance'])
             if CURRENT_USER_INFO['balance'] > (temp + temp * 0.05):
-                CURRENT_USER_INFO['balance'] -= temps
+                CURRENT_USER_INFO['balance'] -= temp
                 CURRENT_USER_INFO['balance'] -= temp * 0.05
 
                 write_record('%s - 储蓄账户：%f；信用卡账户：%f；手续费：%f；对方账号：%s' % ("转账", CURRENT_USER_INFO['saving'], temp, temp * 0.05,tansfer_info['card']))
@@ -161,8 +240,9 @@ def pay_check():
        :return:
        """
     # CURRENT_USER_INFO['debt']='{:.2f}'.format(float(CURRENT_USER_INFO['credit'])-float(CURRENT_USER_INFO['balance']))
+    basic_info = json.load(open(os.path.join(settings.USER_DIR_FOLDER, CURRENT_USER_INFO['card'], "basic_info.json")))
     inp=input("请输入要查账的日期：例如2016_5_10\n>>>").strip()#m每月10号出账单
-    leng=len(CURRENT_USER_INFO['debt'])
+    leng=len(basic_info['debt'])
     msg="""
        卡号：              %s
        账单日期:           %s
@@ -172,8 +252,8 @@ def pay_check():
        当前信用卡剩余额度： %s
     """
     for i in range(leng):
-        if inp == CURRENT_USER_INFO['debt'][i]['date']:
-            print(msg%(CURRENT_USER_INFO['card'],inp,CURRENT_USER_INFO['debt'][leng-1]['total_debt'],CURRENT_USER_INFO['debt'][leng-1]['balance_debt'],CURRENT_USER_INFO['saving'],CURRENT_USER_INFO['balance']))
+        if inp == basic_info['debt'][i]['date']:
+            print(msg%(basic_info['card'],inp,basic_info['debt'][i]['total_debt'],basic_info['debt'][i]['balance_debt'],basic_info['saving'],basic_info['balance']))
             break
     else:
         print("该日期没有账单！")
