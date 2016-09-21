4d2ead4fdb585368d002d2a3c3020dee66bee175
shenyuan <yuanshen106@qq.com>
2016-09-21 17:18:52 +0800

    计算器

---
+++ day7/computer.py
@@ -0,0 +1,12 @@
+#!user/bin/env python
+#-*-coding:utf-8 -*-
+# Author: Sy106
+
+
+def brackets(result1):
+			while True:
+				#8*12+(6-(5*6-2)/77+2)*(3-7)+8
+				re.split("\((中间不包含括号)\)", 表达式, 1)
+				8*12+(6-   5*6-2  /77+2)*(3-7)+8
+				ret = 处理加减乘除(5*6-2 )
+				8*12+(6- ret /77+2)*(3-7)+8
