e0697ca75bb3b0e4875438cb20e57d38acee785d
shenyuan <yuanshen106@qq.com>
2016-09-23 18:39:11 +0800

    计算器

--- day4/day6/s4.py
+++ day4/day6/s4.py
@@ -12,13 +12,35 @@ import re
 # print(r.groupdict()) # 获取模型中匹配到的分组结果
  # 无分组
 origin = "hello alex bcd alex lge alex acd 19"
-r = re.split("a(le)x", origin, 1)
+r = re.split("alex", origin, 1)
 print(r)
 
 # 有分组
 #
-# origin = "hello alex bcd alex lge alex acd 19"
-# r1 = re.split("(alex)", origin, 1)
-# print(r1)
-# r2 = re.split("(al(ex))", origin, 1)
-# print(r2)
+origin = "hello alex bcd alex lge alex acd 19"
+r1 = re.split("(alex)", origin, 1)
+print("r1>>",r1)
+r2 = re.split("(al(e(x)))", origin, 1)
+print("r2>>",r2)
+
+
+def fun(ex):
+    ex= re.findall(r"\((.+)\)", ex[0])
+    return ex
+# 无分组
+origin = "hello alex b(c(r(e))r)d adf+bcd lge acd 19"
+while True:
+    r3 = re.findall(r"\((.+)\)", origin)
+    if r3!=[]:
+       r3=fun(fun(r3))
+       print("r3>>", r3)
+    else:
+        break
+
+
+
+
+# 有分组
+# origin = "hello alex bcd abcd lge acd 19"
+r4 = re.findall("a((\w*\W*)c)(d)", origin)
+print("r4>>",r4)
