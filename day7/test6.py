db3370a4e172efc8568dec2b8250240818e2a00a
shenyuan <yuanshen106@qq.com>
2016-10-20 18:00:02 +0800

    http://t.51cto.com/exam/view?id=1023 模拟人生： 1 定义三个人物，屌丝John，美女Liz，高富帅Peter。 John和Liz大学时是恋人，毕业工作后，Liz傍上了Peter，John伤心之余决定通过自己的努力追回Liz，多年后John通过努力变身高富帅，遇到被甩的Liz，Liz向John提出复合，John说……..（自由发挥） 2. 定义人物信息：性别、年龄、工作、人种、国籍、特长，存款。房、车等信息， 3. 用类实现上述故事中三者的关系和人物变化

--- day7/test6.py
+++ day7/test6.py
@@ -2,7 +2,15 @@
 #-*-coding:utf-8 -*-
 # Author: Sy106
 
-import  datetime
+# import  datetime
+#
+# current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# print(current_time)
 
-current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(current_time)
+class Foo:
+    """ 描述类信息，这是用于看片的神奇 """
+
+    def func(self):
+        pass
+
+print (Foo.__doc__)
--- day7/work7.py
+++ day7/work7.py
@@ -16,35 +16,51 @@ class Person:
         self.house = house
         self.car = car
 
-    def Fall_in_love(self,year,person):
+    def Fall_in_love(self,person,year):
         """注释：谈恋爱，消耗200金钱"""
         person.asset -= 20000 * year
         self.asset = self.asset + 20000 * year
 
     def practice(self,year):
-        """注释：自我修炼，增长200金钱"""
-        self.asset += 50000 * year
-        if year ==1:
-            self.house = ['二室一厅']
-            self.car = ['奥拓']
-        elif year == 3:
-            self.house = ['三室一厅']
-            self.car = ['别克']
-        elif year == 5:
-            self.house = ['四室两厅']
-            self.car = ['宝马']
-        else:
-            self.house = ['别墅']
-            self.car = ['特斯拉']
+        """注释：自我修炼，"""
+        self.age += year
+        if self == diao_si:
+            self.asset += 70000 * year
+            if year ==1:
+                self.house = ['租房']
+                self.car = ['奥拓']
+            elif year == 5:
+                self.house = ['二室一厅']
+                self.car = ['别克']
+            elif year == 10:
+                self.house = ['四室两厅']
+                self.car = ['宝马']
+            else:
+                self.house = ['别墅','三室一厅']
+                self.car = ['特斯拉','宝马']
+        elif self == mei_nv:
+            self.asset += 20000 * year
+        elif self == gao_fu_shuai:
+            self.asset += 100000 * year
 
 
+    def dumped(self,person):
+        person.asset -= 2000
 
-    def dumped(self):
-        self.asset -= 200
 
     def speak(self,content):
-        print(content)
+        print('%s:%s'%(self.name,content))
 
+    def graduate(self, Person):
+        if self == mei_nv:
+            Person.speak("%s:我喜欢上了高富帅%s!木木，我们分手吧！"%(mei_nv.name,gao_fu_shuai.name))
+        elif self == diao_si:
+            Person.dumped(diao_si)
+            Person.speak("%s:苍天啊，可不可以不要这样对我！我一定要努力赚钱,追回%s！"%(diao_si.name,mei_nv.name))
+
+
+    def Many_years_later(self):
+        pass
 
     def detail(self):
         """注释：当前对象的详细情况"""
@@ -56,16 +72,35 @@ class Person:
 
 # #####################  开始游戏  #####################
 
-mei_nv = Person('苍井井', '女', 18,'china', 1000,'reading',[],[])  # 创建苍井井角色
-diao_si = Person('东尼木木', '男', 20,'china', 1800,'singing',[],[])  # 创建屌丝角色
-gao_fu_shuai = Person('尼普', '男', 29, 'china',25000,'swimming',[],[])  # 创建高富帅角色
-
-
-def graduate(self,person):
-    pass
-def Many_years_later():
-    pass
+mei_nv = Person('Liz', '女', 18,'china', 1000,'reading',[],[])  # 创建苍井井角色
+diao_si = Person('John', '男', 18,'china', 1800,'singing',[],[])  # 创建屌丝角色
+gao_fu_shuai = Person('Peter', '男', 29, 'china',2500000,'swimming',['大别墅'],['奔驰'])  # 创建高富帅角色
 
 
 def run():
-    pass
+    Person.detail(mei_nv)
+    Person.detail(diao_si)
+    Person.detail(gao_fu_shuai)
+    print("场景1》》》》》：屌丝和美女刚毕业")
+    Person.graduate(mei_nv, diao_si)
+    Person.graduate(diao_si,mei_nv)
+    Person.detail(mei_nv)
+    Person.detail(diao_si)
+    Person.detail(gao_fu_shuai)
+    print("场景2》》》》》：若干年后。。。")
+    Person.practice(mei_nv,16)
+    Person.practice(diao_si, 16)
+    Person.practice(gao_fu_shuai, 16)
+    Person.Fall_in_love(mei_nv,gao_fu_shuai,10)
+    Person.dumped(mei_nv,gao_fu_shuai)#恋爱10年后，美女被高富帅甩掉
+    print('独白：：恋爱10年后，美女被高富帅甩掉,又过了6年，美女再次遇到当年的屌丝，发现屌丝变为高富帅，提出复合！')
+    print('%s:亲爱的%s,经过16年的打拼，你终于变成高富帅，我们可以重新开始吗？'%(mei_nv.name,diao_si.name))
+    Person.speak(diao_si,'抱歉，当年是你贪图富贵抛弃我，你曾经是我努力的动力。经过16年的努力，我终于成功了。但是好马不吃回头草，我已经有新的女朋友，没必要再跟你一起!')
+
+    Person.detail(mei_nv)
+    Person.detail(diao_si)
+    Person.detail(gao_fu_shuai)
+
+
+
+run()
