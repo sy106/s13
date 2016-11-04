69d8dd7fe7986b73d32da9f1f4ecf02f9989cf85
shenyuan <yuanshen106@qq.com>
2016-11-04 18:49:01 +0800

    http://t.51cto.com/exam/view?id=1023 模拟人生： 1 定义三个人物，屌丝John，美女Liz，高富帅Peter。 John和Liz大学时是恋人，毕业工作后，Liz傍上了Peter，John伤心之余决定通过自己的努力追回Liz，多年后John通过努力变身高富帅，遇到被甩的Liz，Liz向John提出复合，John说……..（自由发挥） 2. 定义人物信息：性别、年龄、工作、人种、国籍、特长，存款。房、车等信息， 3. 用类实现上述故事中三者的关系和人物变化

--- day09/socket/FTP/userinfo.py
+++ day09/socket/FTP/userinfo.py
@@ -15,13 +15,17 @@ class userinfo(object):
         dic_user = [{'username': self.username, 'password': commons.md5(self.password),"login_time":self.time}]
         json.dump(dic_user,open('user_db'),'wb')
 
-    def user_create(name,passwd = ''):
-        if
+def user_create(NAME,PASSWD = ''):
+    if os.path.exists('user_db'):
         user_dict = json.load(open( 'user_db'), 'rb')
         len = len(user_dict)
         for i in range(len):
-            if name == user_dict[i]['username'] and commons.md5(passwd) == user_dict[i]['password']:
-                    result = userinfo(name,passwd,time.time())
+            if NAME == user_dict[i]['username'] and commons.md5(PASSWD) == user_dict[i]['password']:
+                    result = userinfo(NAME,PASSWD,time.time())
                     result.save_user()
+    else:
+        print("the user is not exit !")
 
+if __name__ == '__main__':
+    user_create(name,password)
 
